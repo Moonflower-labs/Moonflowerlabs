e35059b08c4fe2c93493448857f151d0f5757e0b
Moonflower-labs <134706556+Moonflower-labs@users.noreply.github.com>
2023-08-09 15:12:18 +0100

    fix

--- notesApp/scripts/main.js
+++ notesApp/scripts/main.js
@@ -3,3 +3,10 @@ import App from "./app.js";
 
 const root = document.getElementById("app");
 const app = new App(root);
+
+const popoverTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="popover"]'
+);
+const popoverList = Array.from(popoverTriggerList).map(
+  (popoverTriggerEl) => new bootstrap.Popover(popoverTriggerEl)
+);
