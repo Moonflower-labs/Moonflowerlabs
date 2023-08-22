050a0e49a597560583635b42c4e1342da0215db3
Moonflower-labs <134706556+Moonflower-labs@users.noreply.github.com>
2023-08-22 13:55:54 +0100

    updates

--- scripts/main.js
+++ scripts/main.js
@@ -1,35 +1,39 @@
-//* fetching news
-const newsApi =
-  "https://newsdata.io/api/1/news?apikey=pub_2558436bc412f72d26d9e8b9c46242bccbde2&q=Software%20crypto&language=en&category=technology";
+// //* fetching news
+// const newsApi =
+//   "https://newsdata.io/api/1/news?apikey=pub_2558436bc412f72d26d9e8b9c46242bccbde2&q=Software%20crypto&language=en&category=technology";
 
-async function getNews() {
-  const response = await fetch(newsApi);
-  const newsData = await response.json();
-  return newsData;
-}
+// async function getNews() {
+//   const response = await fetch(newsApi);
+//   const newsData = await response.json();
+//   return newsData;
+// }
 
-const newsDiv = document.querySelector(".news");
+// const newsDiv = document.querySelector(".news");
 
-document.addEventListener("DOMContentLoaded", async () => {
-  let newData1 = [];
-  newData1 = await getNews();
-  console.log(newData1);
+// document.addEventListener("DOMContentLoaded", async () => {
+//   let newData1 = [];
+//   newData1 = await getNews();
+//   console.log(newData1);
 
-  const newOut = newData1.results;
+//   const newOut = newData1.results;
 
-  const newsItems = newOut.map(({ title, creator, pubDate }) => {
-    const newsItem = document.createElement("div");
-    newsItem.classList.add("display-4", "mx-5");
-    if (creator === null) {
-      newsItem.innerHTML = `${title}, ${pubDate}`;
-    } else {
-      newsItem.innerHTML = `${title}, ${creator}, ${pubDate}`;
-    }
+//   const newsItems = newOut.map(({ title, creator, pubDate }) => {
+//     const newsItem = document.createElement("div");
+//     newsItem.classList.add("display-4", "mx-5");
+//     if (creator === null) {
+//       newsItem.innerHTML = `${title}, ${pubDate}`;
+//     } else {
+//       newsItem.innerHTML = `${title}, ${creator}, ${pubDate}`;
+//     }
 
-    return newsItem;
-  });
+//     return newsItem;
+//   });
 
-  // newsItems.forEach((newsItem) => {
-  //   newsDiv.appendChild(newsItem);
-  // });
+//   // newsItems.forEach((newsItem) => {
+//   //   newsDiv.appendChild(newsItem);
+//   // });
+// });
+document.querySelector(".card").addEventListener("click", function () {
+  // Toggle the 'flipped' class on the card element
+  this.classList.toggle("flipped");
 });
