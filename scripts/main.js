e619f2dae95cfa8dd76a6d1d8e3d3a7d1a30ff23
Moonflower-labs <134706556+Moonflower-labs@users.noreply.github.com>
2025-04-14 23:38:06 +0100

    Update main.js redirect

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-window.location.href = "https://portfolio-42z.pages.dev/";
+window.location.href = "http://moonflowerlabs.vercel.app";
 // //* fetching news
 // const newsApi =
 //   "https://newsdata.io/api/1/news?apikey=pub_2558436bc412f72d26d9e8b9c46242bccbde2&q=Software%20crypto&language=en&category=technology";
